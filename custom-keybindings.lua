013d835eeee9d6935361088dc918783550159e7a
Sujan Basnet <basnetsujan95@gmail.com>
2025-06-24 17:51:00 +0545

    overwritten bindings plus custom keybindings

---
+++ custom-keybindings.lua
@@ -0,0 +1,28 @@
+local keymap = vim.keymap
+
+-- explorer
+keymap.set('n', '<Leader>pv', vim.cmd.Ex)
+
+-- open terminal in a new tab
+keymap.set('n', '<Leader>tt', ':tabnew | term<Cr> :startinsert<Cr>')
+
+-- open terminal as a new vertical window
+keymap.set('n', '<C-t>', ':vsp | term<Cr> :vert res 75<Cr> :startinsert<Cr>')
+
+-- show diagnostics in popup dialog
+vim.api.nvim_set_keymap('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+
+-- Disable virtual_text, using float window
+vim.diagnostic.config {
+  virtual_text = false,
+}
+
+-- code actions
+keymap.set('n', '<Leader>ca', function()
+  vim.lsp.buf.code_action()
+end)
+
+-- gd with a vertical split
+vim.api.nvim_set_keymap('n', '<C-g><C-d>', ':vsp | :lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+
+keymap.set('n', '<Leader>rc', ':-1read $HOME/.config/nvim/snippets/react-component.tsx<CR>')
--- init.lua
+++ init.lua
@@ -166,6 +166,9 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- relative line numbers
+vim.opt.relativenumber = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -428,10 +431,10 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = '[P]roject [W]ord' })
+      vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[P]roject [S]earch' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -973,12 +976,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -1011,6 +1014,8 @@ require('lazy').setup({
     },
   },
 })
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- added by me
+require 'custom-keybindings'
