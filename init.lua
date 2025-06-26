4d9cb527887cbd2d77ada979a4ce5e31ddaa1fcc
Sujan Basnet <basnetsujan95@gmail.com>
2025-06-26 12:03:15 +0545

    shows hidden files in telescope.find_files

--- init.lua
+++ init.lua
@@ -431,7 +431,9 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [F]iles' })
+      vim.keymap.set('n', '<leader>pf', function()
+        builtin.find_files { hidden = true }
+      end, { desc = '[P]roject [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = '[P]roject [W]ord' })
       vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = '[P]roject [S]earch' })
